a72078993ed6b0e212f8dfca0d7459be059474bc
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-09-23 09:07:58 +0200

    adds the top elements of the stack

--- math.c
+++ math.c
@@ -9,7 +9,7 @@ void add(stack_t **head, unsigned int line_number)
 {
 	stack_t *temp;
 
-	if (head == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
